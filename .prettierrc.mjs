90c455f6324306bc8699f236020e7d91090459bd
jaluik <cqzhengmouren@gmail.com>
2024-01-08 16:30:37 +0800

    chore: 项目初始化

---
+++ .prettierrc.mjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Options} */
+const config = {
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: true,
+};
+export default config;
