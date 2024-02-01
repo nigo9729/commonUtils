8f19055c916117246415ae82af96fed9b9b862cb
jaluik <cqzhengmouren@gmail.com>
2024-02-01 14:09:53 +0800

    feat: 调整测试脚本

--- packages/utils/jest.config.ts
+++ packages/utils/jest.config.ts
@@ -2,6 +2,14 @@ import type { Config } from 'jest';
 
 const config: Config = {
   collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 95,
+      lines: 95,
+      statements: 95,
+    },
+  },
 };
 
 export default config;
