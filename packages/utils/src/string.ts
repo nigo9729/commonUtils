24831d698c16bd774650b47416cc9fac3dee0c0b
nigo <chenzhuo9229@gmail.com>
2024-02-20 16:32:04 +0800

    feat: update

--- packages/utils/src/string.ts
+++ packages/utils/src/string.ts
@@ -38,3 +38,51 @@ export const desensitizeIdNum = (str?: string): string => {
   const sep = '*'.repeat(Math.min(length - 4, 10));
   return str.substring(0, 4) + sep + str.substring(14);
 };
+
+/**
+ * 性别转换
+ * @param str 身份证号
+ * @returns 比如 男 M 1 => 男
+ */
+export const translateGender = (str?: string | number): string => {
+  // 特别处理 null 和 undefined，以及空字符串
+  if (str === null || str === undefined || str === '') {
+    return '未知';
+  }
+  // 确保数字0被正确处理，先转换为字符串
+  const input = str.toString();
+  switch (input) {
+    case 'M':
+    case '男':
+    case '1':
+      return '男';
+    case 'F':
+    case '女':
+    case '0':
+      return '女';
+    default:
+      return '未知';
+  }
+};
+
+/**
+ * 尝试解析 JSON 字符串。
+ *
+ * @param value 需要解析的字符串。
+ * @returns 如果字符串是有效的 JSON 格式，则返回解析后的对象或数组；否则返回原始字符串。
+ */
+export const parseJSON = (value: any): any => {
+  if (!value) {
+    return '';
+  }
+
+  if (typeof value !== 'string') {
+    return value;
+  }
+
+  try {
+    return JSON.parse(value);
+  } catch (_) {
+    return value;
+  }
+};
